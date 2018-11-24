0f142eb8a6a828733d9345b3280011c334cd8392
Isaac <Isaac2020>
2018-11-24 08:08:45 +0100

    4nd Commit

--- app/src/main/java/com/gdgminna/android/ncdevfestgdgminna/activities/MainActivity.java
+++ app/src/main/java/com/gdgminna/android/ncdevfestgdgminna/activities/MainActivity.java
@@ -3,8 +3,6 @@ package com.gdgminna.android.ncdevfestgdgminna.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.gdgminna.android.ncdevfestgdgminna.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -91,15 +90,15 @@ public class MainActivity extends AppCompatActivity
             // Handle the camera action
         } else if (id == R.id.nav_list) {
             startActivity(new Intent(this, EntryListActivity.class));
-            finish();
+//            finish();
 
         } else if (id == R.id.nav_my_detail) {
             startActivity(new Intent(this, MyDetailsActivity.class));
-            finish();
+//            finish();
 
         } else if (id == R.id.nav_manage) {
             startActivity(new Intent(this, NewEntryActivity.class));
-            finish();
+//            finish();
 
         } else if (id == R.id.nav_share) {
 
